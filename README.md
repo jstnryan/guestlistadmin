1b9d9a7811ae3a7ec9c42cf2e61c7068f9248790
Justin Ryan <jstn@jstnryan.com>
2017-01-19 04:51:25 -0700

    DB Schema Update
    Added user permission roles, list signup limits, group administrators
    ("promoters")

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Multi-organization, multi-user guest list collection, tracking, and check-in (with integrated billing considerations)
 
 **NOT FOR PUBLIC CONSUMPTION**
+
+### Dependencies:
+- Apache
+- PHP
+- MySQL
+- Slim Framework
--- _db_setup/db_create.sql
+++ _db_setup/db_create.sql
@@ -1,17 +1,19 @@
 /* guestlistadmin "version 2" - empty database creation file */
 
 CREATE TABLE `organizations` (
-	`id` INT NOT NULL PRIMARY KEY,
-	`name` VARCHAR(255) NOT NULL
+	`id` INT NOT NULL,
+	`name` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `events` (
-	`id` INT NOT NULL PRIMARY KEY,
+	`id` INT NOT NULL,
 	`organization` INT NOT NULL,
 	`name` varchar(255) NOT NULL,
 	`venue` INT NOT NULL,
 	`start` DATETIME,
-	`end` DATETIME
+	`end` DATETIME,
+	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `venues` (
@@ -20,17 +22,18 @@ CREATE TABLE `venues` (
 	`address` varchar(255),
 	`organization` INT NOT NULL,
 	`timezone` varchar(255),
+	`currency` INT NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `users` (
 	`id` INT NOT NULL,
-	`organization` varchar(255),
+	`organization` INT,
 	`name` varchar(255) NOT NULL,
-	`email` varchar(255) UNIQUE,
+	`email` varchar(255),
 	`password` varchar(64),
 	`password_salt` varchar(32) UNIQUE,
-	`groups` varchar(255),
+	`role` INT NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
@@ -38,7 +41,6 @@ CREATE TABLE `groups` (
 	`id` INT NOT NULL,
 	`name` varchar(255) NOT NULL,
 	`organization` INT,
-	`administrators` varchar(255),
 	PRIMARY KEY (`id`)
 );
 
@@ -47,28 +49,18 @@ CREATE TABLE `lists` (
 	`event` INT NOT NULL,
 	`valid_start` DATETIME,
 	`valid_end` DATETIME,
-	`age` INT,
-	`price` INT,
-	`users` varchar(255),
-	`groups` varchar(255),
 	`signup_valid` BOOLEAN NOT NULL DEFAULT 'false',
 	`signup_limit` INT NOT NULL DEFAULT '-1',
 	`status` INT NOT NULL,
-	`gender` INT NOT NULL,
-	PRIMARY KEY (`id`)
-);
-
-CREATE TABLE `prices` (
-	`id` INT NOT NULL,
-	`organization` INT NOT NULL,
-	`name` varchar(255) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `ages` (
 	`id` INT NOT NULL,
 	`name` varchar(255) NOT NULL,
-	`organization` INT NOT NULL,
+	`organization` INT,
+	`age_low` INT,
+	`age_high` INT,
 	PRIMARY KEY (`id`)
 );
 
@@ -108,14 +100,15 @@ CREATE TABLE `settings` (
 	`organization` INT NOT NULL,
 	`timezone` varchar(255),
 	`date_format` varchar(255),
-	`short_url` varchar(255),
+	`domain` INT,
 	`event_start` TIME,
 	`event_end` TIME,
 	`list_expiration` TIME,
+	`currency` INT,
 	PRIMARY KEY (`id`)
 );
 
-CREATE TABLE `shortlink` (
+CREATE TABLE `shortlinks` (
 	`id` INT NOT NULL,
 	`link` varchar(255) NOT NULL UNIQUE,
 	PRIMARY KEY (`id`)
@@ -156,35 +149,124 @@ CREATE TABLE `shortlink_user` (
 	PRIMARY KEY (`id`)
 );
 
+CREATE TABLE `domains` (
+	`id` INT NOT NULL,
+	`organization` INT NOT NULL,
+	`domain` varchar NOT NULL UNIQUE,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `group_members` (
+	`id` INT NOT NULL,
+	`user` INT NOT NULL,
+	`group` INT NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `list_users` (
+	`id` INT NOT NULL,
+	`list` INT NOT NULL,
+	`user` INT NOT NULL,
+	`signup_valid` BOOLEAN NOT NULL DEFAULT 'false',
+	`signup_limit` INT DEFAULT '-1',
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `list_groups` (
+	`id` INT NOT NULL,
+	`list` INT NOT NULL,
+	`group` INT NOT NULL,
+	`signup_valid` BOOLEAN NOT NULL DEFAULT 'false',
+	`signup_limit_group` INT NOT NULL DEFAULT '-1',
+	`signup_limit_user` INT NOT NULL DEFAULT '-1',
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `currencies` (
+	`id` INT NOT NULL,
+	`code` varchar(3) NOT NULL UNIQUE,
+	`name` varchar(255) NOT NULL UNIQUE,
+	`symbol` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `list_constraint` (
+	`id` INT NOT NULL,
+	`time_begin` TIME,
+	`time_end` TIME,
+	`gender` INT,
+	`age` INT,
+	`price` DECIMAL NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `list_constraint_group` (
+	`id` INT NOT NULL,
+	`name` varchar(255) NOT NULL,
+	`organization` INT NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `list_constraints` (
+	`id` INT NOT NULL,
+	`constraint_group` INT NOT NULL,
+	`constraint` INT NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `list_constraint_groups` (
+	`id` INT NOT NULL,
+	`list` INT NOT NULL,
+	`constraint_group` INT NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `themes` (
+	`id` INT NOT NULL,
+	`organization` INT NOT NULL,
+	`name` varchar(255) NOT NULL,
+	`filename` varchar(255) NOT NULL UNIQUE,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `group_administrators` (
+	`id` INT NOT NULL,
+	`group` INT NOT NULL,
+	`user` INT NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `user_permissions` (
+	`id` INT NOT NULL,
+	`bit` BINARY NOT NULL UNIQUE,
+	`name` varchar NOT NULL UNIQUE DEFAULT '255',
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `user_roles` (
+	`id` INT NOT NULL,
+	`name` varchar NOT NULL UNIQUE DEFAULT '255',
+	`permissions` BINARY NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
 ALTER TABLE `events` ADD CONSTRAINT `events_fk0` FOREIGN KEY (`organization`) REFERENCES `organizations`(`id`);
 
 ALTER TABLE `events` ADD CONSTRAINT `events_fk1` FOREIGN KEY (`venue`) REFERENCES `venues`(`id`);
 
 ALTER TABLE `venues` ADD CONSTRAINT `venues_fk0` FOREIGN KEY (`organization`) REFERENCES `organizations`(`id`);
 
+ALTER TABLE `venues` ADD CONSTRAINT `venues_fk1` FOREIGN KEY (`currency`) REFERENCES `currencies`(`id`);
+
 ALTER TABLE `users` ADD CONSTRAINT `users_fk0` FOREIGN KEY (`organization`) REFERENCES `organizations`(`id`);
 
-ALTER TABLE `users` ADD CONSTRAINT `users_fk1` FOREIGN KEY (`groups`) REFERENCES `groups`(`id`);
+ALTER TABLE `users` ADD CONSTRAINT `users_fk1` FOREIGN KEY (`role`) REFERENCES `user_roles`(`id`);
 
 ALTER TABLE `groups` ADD CONSTRAINT `groups_fk0` FOREIGN KEY (`organization`) REFERENCES `organizations`(`id`);
 
-ALTER TABLE `groups` ADD CONSTRAINT `groups_fk1` FOREIGN KEY (`administrators`) REFERENCES `users`(`id`);
-
 ALTER TABLE `lists` ADD CONSTRAINT `lists_fk0` FOREIGN KEY (`event`) REFERENCES `events`(`id`);
 
-ALTER TABLE `lists` ADD CONSTRAINT `lists_fk1` FOREIGN KEY (`age`) REFERENCES `ages`(`id`);
-
-ALTER TABLE `lists` ADD CONSTRAINT `lists_fk2` FOREIGN KEY (`price`) REFERENCES `prices`(`id`);
-
-ALTER TABLE `lists` ADD CONSTRAINT `lists_fk3` FOREIGN KEY (`users`) REFERENCES `users`(`id`);
-
-ALTER TABLE `lists` ADD CONSTRAINT `lists_fk4` FOREIGN KEY (`groups`) REFERENCES `groups`(`id`);
-
-ALTER TABLE `lists` ADD CONSTRAINT `lists_fk5` FOREIGN KEY (`status`) REFERENCES `list_status`(`id`);
-
-ALTER TABLE `lists` ADD CONSTRAINT `lists_fk6` FOREIGN KEY (`gender`) REFERENCES `genders`(`id`);
-
-ALTER TABLE `prices` ADD CONSTRAINT `prices_fk0` FOREIGN KEY (`organization`) REFERENCES `organizations`(`id`);
+ALTER TABLE `lists` ADD CONSTRAINT `lists_fk1` FOREIGN KEY (`status`) REFERENCES `list_status`(`id`);
 
 ALTER TABLE `ages` ADD CONSTRAINT `ages_fk0` FOREIGN KEY (`organization`) REFERENCES `organizations`(`id`);
 
@@ -204,14 +286,52 @@ ALTER TABLE `checkins` ADD CONSTRAINT `checkins_fk2` FOREIGN KEY (`guest`) REFER
 
 ALTER TABLE `settings` ADD CONSTRAINT `settings_fk0` FOREIGN KEY (`organization`) REFERENCES `organizations`(`id`);
 
+ALTER TABLE `settings` ADD CONSTRAINT `settings_fk1` FOREIGN KEY (`domain`) REFERENCES `domains`(`id`);
+
+ALTER TABLE `settings` ADD CONSTRAINT `settings_fk2` FOREIGN KEY (`currency`) REFERENCES `currencies`(`id`);
+
 ALTER TABLE `guest_fields` ADD CONSTRAINT `guest_fields_fk0` FOREIGN KEY (`organization`) REFERENCES `organizations`(`id`);
 
 ALTER TABLE `guest_fields` ADD CONSTRAINT `guest_fields_fk1` FOREIGN KEY (`type`) REFERENCES `guest_field_type`(`id`);
 
-ALTER TABLE `shortlink_list` ADD CONSTRAINT `shortlink_list_fk0` FOREIGN KEY (`link_id`) REFERENCES `shortlink`(`id`);
+ALTER TABLE `shortlink_list` ADD CONSTRAINT `shortlink_list_fk0` FOREIGN KEY (`link_id`) REFERENCES `shortlinks`(`id`);
 
 ALTER TABLE `shortlink_list` ADD CONSTRAINT `shortlink_list_fk1` FOREIGN KEY (`list`) REFERENCES `lists`(`id`);
 
-ALTER TABLE `shortlink_user` ADD CONSTRAINT `shortlink_user_fk0` FOREIGN KEY (`link`) REFERENCES `shortlink`(`id`);
+ALTER TABLE `shortlink_user` ADD CONSTRAINT `shortlink_user_fk0` FOREIGN KEY (`link`) REFERENCES `shortlinks`(`id`);
 
 ALTER TABLE `shortlink_user` ADD CONSTRAINT `shortlink_user_fk1` FOREIGN KEY (`user`) REFERENCES `users`(`id`);
+
+ALTER TABLE `domains` ADD CONSTRAINT `domains_fk0` FOREIGN KEY (`organization`) REFERENCES `organizations`(`id`);
+
+ALTER TABLE `group_members` ADD CONSTRAINT `group_members_fk0` FOREIGN KEY (`user`) REFERENCES `users`(`id`);
+
+ALTER TABLE `group_members` ADD CONSTRAINT `group_members_fk1` FOREIGN KEY (`group`) REFERENCES `groups`(`id`);
+
+ALTER TABLE `list_users` ADD CONSTRAINT `list_users_fk0` FOREIGN KEY (`list`) REFERENCES `lists`(`id`);
+
+ALTER TABLE `list_users` ADD CONSTRAINT `list_users_fk1` FOREIGN KEY (`user`) REFERENCES `users`(`id`);
+
+ALTER TABLE `list_groups` ADD CONSTRAINT `list_groups_fk0` FOREIGN KEY (`list`) REFERENCES `lists`(`id`);
+
+ALTER TABLE `list_groups` ADD CONSTRAINT `list_groups_fk1` FOREIGN KEY (`group`) REFERENCES `groups`(`id`);
+
+ALTER TABLE `list_constraint` ADD CONSTRAINT `list_constraint_fk0` FOREIGN KEY (`gender`) REFERENCES `genders`(`id`);
+
+ALTER TABLE `list_constraint` ADD CONSTRAINT `list_constraint_fk1` FOREIGN KEY (`age`) REFERENCES `ages`(`id`);
+
+ALTER TABLE `list_constraint_group` ADD CONSTRAINT `list_constraint_group_fk0` FOREIGN KEY (`organization`) REFERENCES `organizations`(`id`);
+
+ALTER TABLE `list_constraints` ADD CONSTRAINT `list_constraints_fk0` FOREIGN KEY (`constraint_group`) REFERENCES `list_constraint_group`(`id`);
+
+ALTER TABLE `list_constraints` ADD CONSTRAINT `list_constraints_fk1` FOREIGN KEY (`constraint`) REFERENCES `list_constraint`(`id`);
+
+ALTER TABLE `list_constraint_groups` ADD CONSTRAINT `list_constraint_groups_fk0` FOREIGN KEY (`list`) REFERENCES `lists`(`id`);
+
+ALTER TABLE `list_constraint_groups` ADD CONSTRAINT `list_constraint_groups_fk1` FOREIGN KEY (`constraint_group`) REFERENCES `list_constraint_group`(`id`);
+
+ALTER TABLE `themes` ADD CONSTRAINT `themes_fk0` FOREIGN KEY (`organization`) REFERENCES `organizations`(`id`);
+
+ALTER TABLE `group_administrators` ADD CONSTRAINT `group_administrators_fk0` FOREIGN KEY (`group`) REFERENCES `groups`(`id`);
+
+ALTER TABLE `group_administrators` ADD CONSTRAINT `group_administrators_fk1` FOREIGN KEY (`user`) REFERENCES `users`(`id`);
