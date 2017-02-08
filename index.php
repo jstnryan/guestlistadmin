3663ac06731603a5933c7a0aafcde443d6be7206
Justin Ryan <jstn@jstnryan.com>
2017-02-07 22:13:09 -0700

    Further branding sanitization
    Removal of all Beta references, generic header, URL references removed

--- index.php
+++ index.php
@@ -41,15 +41,10 @@
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="shortcut icon" href="http://tech.betanightclub.com/favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
     <meta name="author" content="Justin Ryan, justin@betanightclub.com" />
-    <title>Beta Nightclub Guest List Form Administration Utility</title>
+    <title>Club Data Tools - Guest List Form Administration Utility</title>
     <link href='glstyles.css' type='text/css' rel='stylesheet' />
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-      ga('create', 'UA-8872962-5', 'auto');
-      ga('send', 'pageview');
-    </script>
   </head>
   <body>
     <div id="centeredcontent">
